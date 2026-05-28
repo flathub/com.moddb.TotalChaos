7b6ebca9df1ba85f3fb4fec4e71f2996677c000c
mbugni <massi.ergosum@gmail.com>
2026-05-28 11:50:29 +0200

    Runtime 25.08 plus UZDoom

---
+++ totalchaos.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+if [ -f ~/.config/gzdoom/gzdoom.ini ]; then
+    if [ ! -f /var/config/uzdoom/uzdoom.ini ]; then
+        echo "Copying existing gzdoom.ini to uzdoom.ini"
+        mkdir -p /var/config/uzdoom
+        cp ~/.config/gzdoom/gzdoom.ini /var/config/uzdoom/uzdoom.ini
+    fi
+fi
+
+if [ ! -f /var/config/uzdoom/uzdoom.ini ]; then
+    echo "Copying default gzdoom_portable.ini to uzdoom.ini"
+    mkdir -p /var/config/uzdoom
+    cp /app/share/games/doom/gzdoom_portable.ini /var/config/uzdoom/uzdoom.ini
+fi
+
+# Run game engine
+exec uzdoom -file totalchaos.pk3 +fluid_patchset /app/share/games/doom/soundfonts/gzdoom.sf2 "$@"
